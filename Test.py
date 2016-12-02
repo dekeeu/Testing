de7949489c7a5c5ad5c5115c5b5599a286dcd10d
Alexandru Coltuneac <dekeeu@gmail.com>
2016-12-02 18:58:57 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 from httplib import *
 from urlparse import *
 
+import argparse
+
 import sys
 import nmap
 import socket
@@ -38,10 +40,16 @@ class CustomEx(Exception):
         def getMessage(self):
             return self._message
         
+    class ScanException:
+        def __init__(self, message):
+            self._message = message
+            
+        def getMessage(self):
+            return self._message
 # Main class
 
 class Testing:
-    def __init__(self, domain):
+    def __init__(self, domain, new):
         self._httpPort = 80
         self._httpsPort = 443
         
@@ -53,8 +61,11 @@ class Testing:
         
         self._sslLabsApiUrl = 'https://api.ssllabs.com/api/v2/'
         
+        self._new = new
+        
     def start(self):
         try:
+            self.checkArguments()
             self.checkUP()
             self.checkRedirect()
             self.checkCertificate()
@@ -63,6 +74,12 @@ class Testing:
             print(e.getMessage())
         except socket.gaierror as e:
             print(e)
+        except CustomEx.ScanException as e:
+            print(e.getMessage())
+            
+    def checkArguments(self):
+        if '-n' in sys.argv:
+            self._new = True
             
     def checkUP(self):
         _ports = [self._httpPort, self._httpsPort]
@@ -116,7 +133,9 @@ class Testing:
         
         _conn = requests.head(self._unsecureScheme + self._domain + self._defaultPath, allow_redirects = True)
         
-        if _conn.history[0].status_code not in [300, 301, 302, 303, 304, 305, 306, 307, 308]:
+        if len(_conn.history) == 0:
+        #if _conn.is_redirect == False:
+        #if _conn.history[0].status_code not in [300, 301, 302, 303, 304, 305, 306, 307, 308]:
             print(bcolors.FAIL + 'Site is still running on http://' + bcolors.ENDC)
             
         else:
@@ -133,64 +152,132 @@ class Testing:
     def checkCertStatus(self):
         _isDone = False
         _req = requests.get(self._sslLabsApiUrl + '/analyze?host=%s&all=done' % self._domain)
-        
         _result = json.loads(_req.text)
         
         if _result['status'] == 'READY':
             _isDone = True
         
-        return (_isDone,_result)
+        return {'done':_isDone, 'result':_result}
     
-    def do_every(self, done):
+    def do_every(self):
+        print('Current status: ' + bcolors.WARNING + 'In progress' + bcolors.ENDC)
         check = self.checkCertStatus()
         
-        if done == False:
+        while check['done'] == False:
+            time.sleep(5)
+            check = self.checkCertStatus()
             print('Current status: ' + bcolors.WARNING + 'In progress' + bcolors.ENDC)
-            done = check[0]
-            threading.Timer(10, self.do_every,[done]).start()
-        else:
-            print('Current status:' + bcolors.OKBLUE + ' Done' + bcolors.ENDC)
-            return check[1]
-            
+        
+        print('Current status:' + bcolors.OKBLUE + ' Done' + bcolors.ENDC)
+        return check['result']
+     
     def checkCertificate(self):
         print('----- SSL/TLS Checking ----- ')
         
-        self.launchCertScan()
-        _scanResult = self.do_every(False)
+        if self._new:
+            self.launchCertScan()
+        _scanResult = self.do_every()
         
-        _info = _scanResult['endpoints'][0]['details']['cert']
-        _statusList = {0 : bcolors.WARNING + 'not checked' + bcolors.ENDC,
-                       1 : bcolors.FAIL + 'certificate revoked' + bcolors.ENDC,
-                       2 : bcolors.OKGREEN + 'certificate not revoked' + bcolors.ENDC,
-                       3 : bcolors.WARNING + 'revocation check error' + bcolors.ENDC,
-                       4 : bcolors.WARNING + 'no revocation information' + bcolors.ENDC,
-                       5 : bcolors.WARNING + 'internal error' + bcolors.ENDC}
+        _statusList = {0 : bcolors.WARNING + 'NOT checked' + bcolors.ENDC,
+                           1 : bcolors.FAIL + 'certificate revoked' + bcolors.ENDC,
+                           2 : bcolors.OKGREEN + 'certificate NOT revoked' + bcolors.ENDC,
+                           3 : bcolors.WARNING + 'revocation check error' + bcolors.ENDC,
+                           4 : bcolors.WARNING + 'no revocation information' + bcolors.ENDC,
+                           5 : bcolors.WARNING + 'internal error' + bcolors.ENDC}
         
+        _grades = {'A' : bcolors.OKGREEN + 'A' + bcolors.ENDC,
+                   'A+' : bcolors.OKGREEN + 'A' + bcolors.ENDC,
+                   'B' : bcolors.WARNING + 'B' + bcolors.ENDC,
+                   'C' : bcolors.FAIL + 'C' + bcolors.ENDC,
+                   'D' : bcolors.FAIL + 'D' + bcolors.ENDC,
+                   'E' : bcolors.FAIL + 'E' + bcolors.ENDC,
+                   'F' : bcolors.FAIL + 'F' + bcolors.ENDC}
         
-        # http://stackoverflow.com/questions/35822002/url-common-name-matching-python
-        
-        common_name = _info['cert']['commonNames']
-        rxString = r'(?:^|\s)(\w+\.)?' + common_name.replace('.', '\.')[3:] + '(?:$|\s)'
-        regex = re.compile(rxString)
-        
-        if regex.match(self._domain):
-            print("Certificate Name " + bcolors.OKGREEN + " matches " + bcolors.ENDC + self._domain)
-        else:
-            print("Certificate Name does " + bcolors.FAIL + " NOT match " + bcolors.ENDC + self._domain)
-        
-        if _info['notBefore'] <= time.time() <= _info['notAfter']:
-            print('\tCertificate is ' + bcolors.OKGREEN + 'VALID' + bcolors.ENDC)
-        else:
-            print('\tCertificate is ' + bcolors.FAIL + 'EXPIRED' + bcolors.ENDC)
+        for endPoint in _scanResult['endpoints']:
             
-        print('\tRevocation status: ' + _statusList[_info['revocationStatus']])
-        print('\tCrl Revocation status' + _statusList[_info['crlRevocationStatus']])
-        print('\tOCSP Revocation status ' + _statusList[_info['ocspRevocationStatus']])
-        
-        
-        
-        
+            _certInfo = endPoint['details']['cert']
+            _endpointInfo = endPoint['details']
+            
+            print('\nGrade: ' + _grades[endPoint['grade']] + '\n')
+            
+            """ Web server details """
+            
+            print(bcolors.HEADER + 'Web server information' + bcolors.ENDC)
+            
+            print('IP Address:' + endPoint['ipAddress'])
+            
+            
+            """ Certificate Information """
+            
+            print(bcolors.HEADER + 'Certificate information' + bcolors.ENDC)
+            
+            print('Common Names: ' + ','.join(_certInfo['commonNames']))
+            print('Alt Names: ' + ','.join(_certInfo['altNames']))
+            print('Issuer info: ' + _certInfo['issuerLabel'] + '(' + _certInfo['issuerSubject'] + ')')
+            print('SHA1 Thumbprint: ' + _certInfo['sha1Hash'])
+            print('Signature Algorithm: ' + _certInfo['sigAlg'])
+            
+            print '-'*30
+            
     
-for host in sys.argv[1:]:
-    H = Testing(host)
-    H.start()
+            """ Certificate Revoked Status """
+            
+            print('Revocation status: ' + _statusList[_certInfo['revocationStatus']])
+            print('CRL Revocation status: ' + _statusList[_certInfo['crlRevocationStatus']])
+            print('OCSP Revocation status: ' + _statusList[_certInfo['ocspRevocationStatus']])
+            
+            
+            """ SSL Certificate Expiration """
+            
+            if _certInfo['notBefore'] / 1000 <= int(time.time()) <= _certInfo['notAfter'] / 1000:
+                print('Certificate is ' + bcolors.OKGREEN + 'VALID' + bcolors.ENDC)
+            else:
+                print('Certificate is ' + bcolors.FAIL + 'EXPIRED' + bcolors.ENDC)
+                
+                
+            """ SSL Certificate Match Name """
+            # http://stackoverflow.com/questions/35822002/url-common-name-matching-python
+    
+            if _scanResult.has_key('certHostnames') == False:
+                print("Certificate Name " + bcolors.OKGREEN + "matches " + bcolors.ENDC + self._domain)
+            else:
+                print("Certificate Name does " + bcolors.FAIL + "NOT match " + bcolors.ENDC + self._domain)
+                
+                
+            """ Heartbleed Vulnerability """
+            
+            print 'Server vulnerable to HeartBleed:',
+            if _endpointInfo['heartbleed'] == True:
+                print(bcolors.FAIL + str(_endpointInfo['heartbleed']) + bcolors.ENDC)
+            else:
+                print(bcolors.OKGREEN + str(_endpointInfo['heartbleed']) + bcolors.ENDC)
+            
+            
+            """ Protocol Support """
+            print(bcolors.HEADER + 'Supported protocols: ' + bcolors.ENDC)
+            for proto in _endpointInfo['protocols']:
+                print proto['name'] + ', ' + proto['version']
+            
+            
+            """ Chiper Support """
+            
+            print(bcolors.HEADER + 'Supported chipers: ' + bcolors.ENDC)
+            for chip in _endpointInfo['suites']['list']:
+                print chip['name']
+                
+                
+class GUI:
+    def __init__(self):
+        self._parser = argparse.ArgumentParser()
+    
+    def start(self):
+        self._parser.add_argument('-d', nargs='*', help = 'Domains', required = True)
+        self._parser.add_argument('-n', action='store_true', help = 'New scan', required = False)
+        self._args = self._parser.parse_args()
+        
+        for host in self._args.d:
+            H = Testing(host,self._args.n)
+            H.start()
+            
+g = GUI()
+g.start()
